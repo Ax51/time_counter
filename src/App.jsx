2f284aa577900a3eef38688ba2ac4d694bb40331
Ax51 <80718359+Ax51@users.noreply.github.com>
2022-07-15 17:47:30 +0300

    Found active task

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Snackbar from "./utils/Snackbar";
 
 export default function App() {
   const tasks = useStore((state) => state.tasks.tasksArr);
+  const runningTask = tasks.find(
+    (task) => task.periods.filter((period) => !period.endTime).length > 0,
+  );
   const activeTasks = tasks.filter((i) => !i.isArchived);
   const showActiveOnly = useStore((state) => state.tasks.showActiveOnly);
   const renderTasksArr = showActiveOnly ? activeTasks : tasks;
@@ -16,7 +19,7 @@ export default function App() {
   return (
     <Box sx={{ width: "100vw", minHeight: "100vh", bgcolor: "grey.100" }}>
       <Box sx={{ p: 2 }}>
-        <SetupTask />
+        <SetupTask runningTask={runningTask} />
         <Grid container spacing={2}>
           {renderTasksArr.map((i) => (
             <RenderTask key={i.id} task={i} />
