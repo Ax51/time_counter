2a46a7890a342d360fa9f50559c2f31152064010
Ax51 <80718359+Ax51@users.noreply.github.com>
2022-07-16 15:36:37 +0300

    store code optimized

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,8 @@
-/* eslint-disable prettier/prettier */
 import create from "zustand";
 import { persist } from "zustand/middleware";
-import { nanoid } from "nanoid";
 import merge from "deepmerge";
 
-const taskInitialState = {
-  id: String(),
-  name: String(),
-  timestamp: +new Date(),
-  isActive: Boolean(),
-  isArchived: Boolean(),
-  periods: [
-    { startTime: +new Date(), endTime: +new Date() },
-    { startTime: +new Date(), endTime: null },
-  ],
-};
+import { createNewTask, resumePausedTask, stopRunningTask } from "./storeUtils";
 
 // TODO: add zustand lens to separate stores
 
@@ -33,30 +21,10 @@ export const useStore = create(
           })),
         addTask: (givenData) =>
           set((state) => {
-            const oldTasks = state.tasks.tasksArr.map((i) => {
-              const isTaskRunning =
-                i.periods[i.periods.length - 1].endTime === null;
-              const lastStartTime = i.periods[i.periods.length - 1].startTime;
-              return isTaskRunning
-                ? {
-                  ...i,
-                  isActive: false,
-                  periods: [
-                    ...i.periods.slice(0, -1),
-                    { startTime: lastStartTime, endTime: +new Date() },
-                  ],
-                }
-                : i;
-            });
-            const timestamp = givenData.timestamp ?? taskInitialState.timestamp;
-            const newTask = {
-              id: givenData.id ?? nanoid(10),
-              name: givenData.name ?? taskInitialState.name,
-              timestamp,
-              isActive: givenData.isActive ?? taskInitialState.isActive,
-              isArchived: false,
-              periods: [{ startTime: timestamp, endTime: null }],
-            };
+            const oldTasks = state.tasks.tasksArr.map((i) =>
+              i.isActive ? stopRunningTask(i) : i,
+            );
+            const newTask = createNewTask(givenData);
             return {
               tasks: { ...state.tasks, tasksArr: [...oldTasks, newTask] },
             };
@@ -68,63 +36,48 @@ export const useStore = create(
               tasksArr: state.tasks.tasksArr.filter((i) => i.id !== id),
             },
           })),
+        toggleDoneTask: (id) =>
+          set((state) => ({
+            tasks: {
+              ...state.tasks,
+              tasksArr: state.tasks.tasksArr.map((i) => {
+                // Found given task
+                if (i.id === id) {
+                  // Check if it's active and now running
+                  if (i.isActive) {
+                    return {
+                      ...stopRunningTask(i),
+                      isDone: true,
+                    };
+                  }
+                  return {
+                    ...i,
+                    isDone: !i.isDone,
+                  };
+                }
+                return i;
+              }),
+            },
+          })),
         pauseTask: (id) =>
-          set((state) => {
-            const taskIndex = state.tasks.tasksArr.findIndex(
-              (i) => i.id === id,
-            );
-            const isNowActive = state.tasks.tasksArr[taskIndex].isActive;
-            return {
-              tasks: {
-                ...state.tasks,
-                tasksArr: state.tasks.tasksArr.map((i) => {
-                  const lastStartTime =
-                    i.periods[i.periods.length - 1].startTime;
-                  const isTaskRunning =
-                    i.periods[i.periods.length - 1].endTime === null;
-                  // found last active task or selected task which is active now
-                  // and pause them
-                  if (isTaskRunning || (i.id === id && isNowActive)) {
-                    const timeLeft = +new Date() - lastStartTime;
-                    const isMoreThanMinute = timeLeft > 60000;
-                    return {
-                      ...i,
-                      isActive: false,
-                      // If last period is less than a minute, so squash it
-                      // with last completed period
-                      periods: isMoreThanMinute || i.periods.length < 2
-                        ? [
-                          ...i.periods.slice(0, -1),
-                          { startTime: lastStartTime, endTime: +new Date() },
-                        ]
-                        : [
-                          ...i.periods.slice(0, -2),
-                          {
-                            startTime:
-                              i.periods[i.periods.length - 2].startTime,
-                            endTime:
-                              i.periods[i.periods.length - 2].endTime +
-                              timeLeft,
-                          },
-                        ],
-                    };
-                  }
-                  // found selected task if it's not active and start new period
-                  if (i.id === id) {
-                    return {
-                      ...i,
-                      isActive: true,
-                      periods: [
-                        ...i.periods,
-                        { startTime: +new Date(), endTime: null },
-                      ],
-                    };
-                  }
-                  return { ...i, isActive: false };
-                }),
-              },
-            };
-          }),
+          set((state) => ({
+            tasks: {
+              ...state.tasks,
+              tasksArr: state.tasks.tasksArr.map((i) => {
+                // found last active task or selected task which is active now
+                // and pause them
+                if (i.isActive) {
+                  return stopRunningTask(i);
+                }
+                // if we hasn't stop before, so this task is not active and,
+                // if it's our task, so we can start it
+                if (i.id === id) {
+                  return resumePausedTask(i);
+                }
+                return i;
+              }),
+            },
+          })),
         renameTask: (id, newName) =>
           set((state) => ({
             tasks: {
@@ -139,69 +92,23 @@ export const useStore = create(
               ),
             },
           })),
-        archiveTask: (id) =>
+        // TODO: swtich to toggleArhiveTask
+        toggleArchiveTask: (id) =>
           set((state) => ({
             tasks: {
               ...state.tasks,
               tasksArr: state.tasks.tasksArr.map((i) =>
                 i.id === id
-                  ? {
-                    ...i,
-                    isArchived: true,
-                    periods: i.isActive
-                      ? [
-                        ...i.periods.slice(0, -1),
-                        {
-                          startTime:
-                            i.periods[i.periods.length - 1].startTime,
-                          endTime: +new Date(),
-                        },
-                      ]
-                      : i.periods,
-                    isActive: false,
-                  }
+                  ? i.isActive
+                    ? {
+                      ...stopRunningTask(i),
+                      isArchived: true,
+                    }
+                    : i.isArchived
+                      ? { ...i, isArchived: false }
+                      : { ...i, isArchived: true }
                   : i,
               ),
-            },
-          })),
-        unarchiveTask: (id) =>
-          set((state) => ({
-            tasks: {
-              ...state.tasks,
-              showActiveOnly: true,
-              tasksArr: state.tasks.tasksArr.map((i) => {
-                const isTaskRunning =
-                  i.periods[i.periods.length - 1].endTime === null;
-                const lastStartTime = i.periods[i.periods.length - 1].startTime;
-                if (isTaskRunning) {
-                  return {
-                    ...i,
-                    isActive: false,
-                    periods: [
-                      ...i.periods.slice(0, -1),
-                      {
-                        startTime: lastStartTime,
-                        endTime: +new Date(),
-                      },
-                    ],
-                  };
-                }
-                if (i.id === id) {
-                  return {
-                    ...i,
-                    isArchived: false,
-                    periods: [
-                      ...i.periods,
-                      {
-                        startTime: +new Date(),
-                        endTime: null,
-                      },
-                    ],
-                    isActive: true,
-                  };
-                }
-                return i;
-              }),
             },
           })),
       },
