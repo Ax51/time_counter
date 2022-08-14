fca5300d0d76ea9b65308d9d625a7eb54b6e68bb
Ax51 <80718359+Ax51@users.noreply.github.com>
2022-08-14 14:21:50 +0300

    created united index file for utils

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export { useTimeout, useInterval } from "./useTimeout";
+export { default as Snackbar } from "./Snackbar";
+export {
+  toHumanTime,
+  stringToMs,
+  humanToMs,
+  relativeToHumanTime,
+  msTimeSpent,
+} from "./time";
